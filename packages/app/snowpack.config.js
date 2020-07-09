291a0655685767ca27218285e63292c64f668ae1
john.brennan <john.brennan@visma.com>
2020-07-09 17:11:42 +0100

    try with esbuild, not working

--- packages/app/snowpack.config.js
+++ packages/app/snowpack.config.js
@@ -1,5 +1,17 @@
+const esbuild = require("rollup-plugin-esbuild");
+
 module.exports = {
-  extends: '@snowpack/app-scripts-react',
-  scripts: {},
-  plugins: [],
+  extends: "@snowpack/app-scripts-react",
+  scripts: {
+  },
+  plugins: ["@snowpack/plugin-react-refresh"],
+  installOptions: {
+    rollup: {
+      plugins: [
+        esbuild({
+          include: ["**/node_modules/@test-package/**"],
+        }),
+      ],
+    },
+  },
 };
--- packages/component/index.jsx
+++ packages/component/index.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
 
 export function Component() {
-  return <div>Hello from dependency.</div>;
+  return <div>Hello from dependency?</div>;
 }
